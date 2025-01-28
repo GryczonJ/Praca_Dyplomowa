943b196454da3db8c14645cdfac0c8f8ad9f353d
GryczonJ <92317770+GryczonJ@users.noreply.github.com>
2025-01-28 13:55:53 +0100

    xDDDDDDD

--- Inzynierka/Inzynierka/Inzynierka/obj/Debug/net8.0/Inzynierka.AssemblyInfo.cs
+++ Inzynierka/Inzynierka/Inzynierka/obj/Debug/net8.0/Inzynierka.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inzynierka")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ee6cac640fd61479beec8d8a69d312b453a5073")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+230979052ec8197ba0b1b26ebaa5452cfb1c6481")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inzynierka")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inzynierka")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
