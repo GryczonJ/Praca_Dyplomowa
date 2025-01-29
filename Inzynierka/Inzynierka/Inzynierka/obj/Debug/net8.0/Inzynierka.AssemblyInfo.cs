d75b4048f52536e0425bac3c82ca5165708141c4
GryczonJ <92317770+GryczonJ@users.noreply.github.com>
2025-01-29 19:48:40 +0100

    nisamowity wygląd straszne

--- Inzynierka/Inzynierka/Inzynierka/obj/Debug/net8.0/Inzynierka.AssemblyInfo.cs
+++ Inzynierka/Inzynierka/Inzynierka/obj/Debug/net8.0/Inzynierka.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inzynierka")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+accddc865ebef7d4215d9033d46001b389f40e45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d420688dc7104609f66cee722b6806aa4ce004f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inzynierka")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inzynierka")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
